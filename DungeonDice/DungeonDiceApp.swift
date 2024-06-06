9c6d8a2e661b3ab317fe024e868ea02bbbc87f6a
Rafael Rodriguez <rafael.nc@me.com>
2024-06-06 10:50:50 -0400

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Rafael Rodriguez on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
