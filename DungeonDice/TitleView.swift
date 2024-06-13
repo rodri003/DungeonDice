0367addaa30ff7e3ac695f2da7e37d0e1be8be8a
Rafael Rodriguez <rafael.nc@me.com>
2024-06-13 11:09:24 -0400

    Ch. 4.6 Checkpoint #1

---
+++ DungeonDice/TitleView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleView.swift
+//  DungeonDice
+//
+//  Created by Rafael Rodriguez on 6/13/24.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleView()
+}
