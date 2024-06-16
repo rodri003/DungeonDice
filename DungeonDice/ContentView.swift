25b5ab35cf77a4f06988313cd58a42e95d8c2445
Rafael Rodriguez <rafael.nc@me.com>
2024-06-16 12:00:27 -0400

    Ch. 4.7 Checkpoint 2

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,31 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    enum Dice: Int, CaseIterable {
-        case four = 4
-        case six = 6
-        case eight = 8
-        case ten = 10
-        case twelve = 12
-        case twenty = 20
-        case hundred = 100
-        
-        func roll() -> Int {
-            return Int.random(in: 1...self.rawValue)
-        }
-    }
     
     @State private var resultMessage = ""
-    @State private var buttonsLeftOver = 0 // # of buttons in a less-than-full row
-    
-    let horizontalPadding: CGFloat = 16 // No swiftUI API to get padding value at time of lesson
-    let spacing: CGFloat = 0 // between buttons
-    let buttonWidth: CGFloat = 102
+
     
     
     
     var body: some View {
-        GeometryReader(content: { geo in
+
             VStack {
                 titleView
                 
@@ -42,33 +25,12 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                buttonLayoutView
+                ButtonLayout(resultMessage: $resultMessage)
                 
             }
             .padding()
-            .onChange(of: geo.size.width, { _, _ in
-                arrangeGridItems(deviceWidth: geo.size.width)
-            })
-            .onAppear(perform: {
-                arrangeGridItems(deviceWidth: geo.size.width)
-            })
-            
-            
-            
-        })
-        
-        
     }
-    
-    func arrangeGridItems(deviceWidth: CGFloat) {
-        var screenWidth = deviceWidth - horizontalPadding*2  // padding on both sides
-        if Dice.allCases.count > 1 {
-            screenWidth += spacing
-            // Calculate numOfButtonsPerRow as an Int
-            let numberOfButtonsPerRow = Int (screenWidth) / Int (buttonWidth + spacing)
-            buttonsLeftOver = Dice.allCases.count % numberOfButtonsPerRow // remainder calculated by %
-        }
-    }
+
 }
 
 extension ContentView {
@@ -87,33 +49,73 @@ extension ContentView {
             .multilineTextAlignment(.center)
             .frame(height: 150)
     }
-    
-    private var buttonLayoutView: some View {
-        VStack {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: buttonWidth), spacing: spacing)], content: {
-                ForEach(Dice.allCases.dropLast(buttonsLeftOver), id:\.self) {dice in
-                    Button("\(dice.rawValue)-sided") {
-                        resultMessage = "You rolled a \(dice.roll()) on a \(dice)-sided dice."
-                    }
-                    .frame(width: buttonWidth)
-                }
-                .buttonStyle(.borderedProminent)
-                .tint(.red)
-            })
-            HStack {
-                ForEach(Dice.allCases.suffix(buttonsLeftOver), id:\.self) {
-                    dice in
-                    Button("\(dice.rawValue)-sided") {
-                        resultMessage = "You rolled a \(dice.roll()) on a \(dice)-sided dice."
-                    }
-                    .frame(width: buttonWidth)
-                }
-                .buttonStyle(.borderedProminent)
-                .tint(.red)
-            }
-        }
-        
+}
 
+struct ButtonLayout: View {
+    enum Dice: Int, CaseIterable {
+        case four = 4
+        case six = 6
+        case eight = 8
+        case ten = 10
+        case twelve = 12
+        case twenty = 20
+        case hundred = 100
+        
+        func roll() -> Int {
+            return Int.random(in: 1...self.rawValue)
+        }
+    }
+    
+    @State private var buttonsLeftOver = 0 // # of buttons in a less-than-full row
+    @Binding var resultMessage: String
+    
+    let horizontalPadding: CGFloat = 16 // No swiftUI API to get padding value at time of lesson
+    let spacing: CGFloat = 0 // between buttons
+    let buttonWidth: CGFloat = 102
+    
+    var body: some View {
+        GeometryReader(content: { geo in
+            VStack {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: buttonWidth), spacing: spacing)], content: {
+                    ForEach(Dice.allCases.dropLast(buttonsLeftOver), id:\.self) {dice in
+                        Button("\(dice.rawValue)-sided") {
+                            resultMessage = "You rolled a \(dice.roll()) on a \(dice)-sided dice."
+                        }
+                        .frame(width: buttonWidth)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
+                })
+                HStack {
+                    ForEach(Dice.allCases.suffix(buttonsLeftOver), id:\.self) {
+                        dice in
+                        Button("\(dice.rawValue)-sided") {
+                            resultMessage = "You rolled a \(dice.roll()) on a \(dice)-sided dice."
+                        }
+                        .frame(width: buttonWidth)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
+                }
+            }
+            .onChange(of: geo.size.width, { _, _ in
+                arrangeGridItems(deviceWidth: geo.size.width)
+            })
+            .onAppear(perform: {
+                arrangeGridItems(deviceWidth: geo.size.width)
+            })
+        })
+
+    }
+    
+    func arrangeGridItems(deviceWidth: CGFloat) {
+        var screenWidth = deviceWidth - horizontalPadding*2  // padding on both sides
+        if Dice.allCases.count > 1 {
+            screenWidth += spacing
+            // Calculate numOfButtonsPerRow as an Int
+            let numberOfButtonsPerRow = Int (screenWidth) / Int (buttonWidth + spacing)
+            buttonsLeftOver = Dice.allCases.count % numberOfButtonsPerRow // remainder calculated by %
+        }
     }
 }
 
